f57d6fc36968fe46de4c536a0341d781937144ca
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-05-12 23:42:26 +0900

    Input Get Value test

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,6 +11,23 @@ public class Player : MonoBehaviour
         // Debug.Log("Hello Player");
         // Input // 유니티의 거의 모든 입력 담당 클레스
         // 어떤 키든 눌려져있으면 1 아니면 0
-        Debug.Log(Input.anyKey);
+        // Debug.Log(Input.anyKey);
+        // (매개변수) 의 키가 눌러지고 있는 동안~ 진행형 bool
+        // Input.GetKeyDown(); // (매개 변수의) 키가 누른 순간 bool
+        // Input.GetKeyUp(); // (매개 변수의) 키가 눌럿다가 때진 순간 bool
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Get Key !");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Get Key Down!");
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Debug.Log("Get Key Up!");
+        }
     }
 }
