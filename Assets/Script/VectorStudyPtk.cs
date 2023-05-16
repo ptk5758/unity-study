d5be2d51e2c5ce273704e8d74baa5d83da040904
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-05-17 01:23:26 +0900

    ptk move to wards

--- Assets/Script/VectorStudyPtk.cs
+++ Assets/Script/VectorStudyPtk.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class VectorStudyPtk : VectorStudy
 {
-    
+    public Vector3 targetPoint;
+    public float speed;
+    public List<Vector3> points;
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) this.targetPoint = points[0];
+        if (Input.GetKeyDown(KeyCode.Alpha2)) this.targetPoint = points[1];
+        if (Input.GetKeyDown(KeyCode.Alpha3)) this.targetPoint = points[2];
+        if (Input.GetKeyDown(KeyCode.Alpha4)) this.targetPoint = points[3];
+        if (Input.GetKeyDown(KeyCode.Alpha5)) this.targetPoint = points[4];
+
+        // Vector3.MoveTowards(현재백터, 목표백터, 스피드)
+        if (Input.GetKey(KeyCode.Space)) {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, targetPoint, speed);
+        }
+        
+    }
 }
