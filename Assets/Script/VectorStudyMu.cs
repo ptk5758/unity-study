0aa58ea0cae453e884e43ec366280be66012ea08
snowplum212 <snowplum212@gmail.com>
2023-05-16 23:17:37 +0900

    mumu

---
+++ Assets/Script/VectorStudyMu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorStudyMu : VectorStudy
+{
+    private void Start()
+    {
+        this.transform.position = new Vector3(3, 3, 3);
+        
+    }
+}
