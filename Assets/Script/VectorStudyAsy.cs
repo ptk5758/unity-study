a40b1941027f365250887daea5400e4734ce6446
asy <asy951013@naver.com>
2023-05-16 23:43:37 +0900

    bb

--- Assets/Script/VectorStudyAsy.cs
+++ Assets/Script/VectorStudyAsy.cs
@@ -7,12 +7,19 @@ public class VectorStudyAsy : VectorStudy
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position = spanwPoint;
+        //this.transform.position = spawnPoint;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            this.transform.position = spawnPoint;
+        }
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            this.transform.position = new Vector3(0,0,0);
+        }
     }
 }
