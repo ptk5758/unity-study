f2510ce2b23c56ff2376baf1ed7c6305e2f358cc
asy <asy951013@naver.com>
2023-05-16 23:18:11 +0900

    aaa

---
+++ Assets/Script/VectorStudyAsy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorStudyAsy : VectorStudy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
