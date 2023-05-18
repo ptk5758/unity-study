96b2743d46931d0e543172d6c54ce3375ff53cf7
ptk5758 <ptk725739@gmail.com>
2023-05-18 09:42:27 +0900

    MoveMent Script add

---
+++ Assets/Script/movement/MoveMent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MoveMent : MonoBehaviour
+{
+    public Vector3[] spawnPoint;
+    public Vector3 targetPoint;
+    public float positionX;
+    public float positionZ;
+    public float speed;
+}
