3d691bb76cf55c84e9657787874d01271814b5bf
ji0ne <zi0neeeeeeeee@gmail.com>
2023-05-21 02:34:42 +0900

    pjwVectorStudy002

--- Assets/Script/movement/MovementPjw.cs
+++ Assets/Script/movement/MovementPjw.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MovementPjw : MoveMent
 {
+    //001커밋이슈 : 각 세이브포인트에 좌표 저장 됨, 1234 키 눌렀을때 정확한 위치로 이동하지않고 그 위치에 머무르지 않음
+    //해결_ update마다 현 위치가 돌아가니 그대로 있질 못함 >> 각 GetKey if문에 위치할당하도록 함
+
+
+    
     void Awake()
     {
         spawnPoint = new Vector3[4];
@@ -15,28 +20,28 @@ public class MovementPjw : MoveMent
         if (Input.GetKey(KeyCode.A))
         {
             positionX -= Time.deltaTime;
-            
+            transform.position = new Vector3(positionX * speed, 0, positionZ * speed);
         }
 
         if (Input.GetKey(KeyCode.D)) 
         {
             positionX += Time.deltaTime;
-
+            transform.position = new Vector3(positionX * speed, 0, positionZ * speed);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             positionZ -= Time.deltaTime;
-
+            transform.position = new Vector3(positionX * speed, 0, positionZ * speed);
         }
 
         if (Input.GetKey(KeyCode.W)) 
         {
             positionZ += Time.deltaTime;
-
+            transform.position = new Vector3(positionX * speed, 0, positionZ * speed);
         }
 
-        transform.position = new Vector3(positionX * speed, 0, positionZ * speed);
+        
         //wasd 이동
 
         if (Input.GetKey(KeyCode.Alpha1)) //키보드 상단 숫자1~4 눌렀을 때
@@ -45,7 +50,7 @@ public class MovementPjw : MoveMent
             if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) //숫자 + shift 눌렀을 때
             {
                 //키 누른 시점의 x,z 좌표를 배열에 저장, 세이브포인트 좌표 출력
-                spawnPoint[0] = new Vector3(positionX, 0, positionZ);
+                spawnPoint[0] = this.transform.position;
                 Debug.Log("세이브포인트 01 : "+spawnPoint[0]);
 
             }
@@ -60,7 +65,7 @@ public class MovementPjw : MoveMent
 
             if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) 
             {
-                spawnPoint[1] = new Vector3(positionX, 0, positionZ);
+                spawnPoint[1] = this.transform.position;
                 Debug.Log("세이브포인트 02 : " + spawnPoint[1]);
             }
 
@@ -74,7 +79,7 @@ public class MovementPjw : MoveMent
 
             if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) 
             {
-                spawnPoint[2] = new Vector3(positionX, 0, positionZ);
+                spawnPoint[2] = this.transform.position;
                 Debug.Log("세이브포인트 03 : " + spawnPoint[2]);
             }
             transform.position = Vector3.MoveTowards(transform.position, spawnPoint[2], speed);
@@ -86,7 +91,7 @@ public class MovementPjw : MoveMent
 
             if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) 
             {
-                spawnPoint[3] = new Vector3(positionX, 0, positionZ);
+                spawnPoint[3] = this.transform.position;
                 Debug.Log("세이브포인트 04 : " + spawnPoint[3]);
             }
 
