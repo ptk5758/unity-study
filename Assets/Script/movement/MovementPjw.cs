49fb73f2f011bfbfb58ac0adc467514c5a88cfe0
ji0ne <zi0neeeeeeeee@gmail.com>
2023-05-21 02:46:13 +0900

    pjwVector003Complete

--- Assets/Script/movement/MovementPjw.cs
+++ Assets/Script/movement/MovementPjw.cs
@@ -6,8 +6,9 @@ public class MovementPjw : MoveMent
 {
     //001커밋이슈 : 각 세이브포인트에 좌표 저장 됨, 1234 키 눌렀을때 정확한 위치로 이동하지않고 그 위치에 머무르지 않음
     //해결_ update마다 현 위치가 돌아가니 그대로 있질 못함 >> 각 GetKey if문에 위치할당하도록 함
+    //MoveTowards가 아닌 SmoothDamp로 마무리함
 
-
+    Vector3 velo = Vector3.zero;
     
     void Awake()
     {
@@ -44,6 +45,7 @@ public class MovementPjw : MoveMent
         
         //wasd 이동
 
+
         if (Input.GetKey(KeyCode.Alpha1)) //키보드 상단 숫자1~4 눌렀을 때
         {
 
@@ -56,7 +58,7 @@ public class MovementPjw : MoveMent
             }
 
             //Movetowards 사용하여 해당 인덱스에 담긴 spawnPoint로 이동
-            transform.position = Vector3.MoveTowards(transform.position, spawnPoint[0], speed);
+            transform.position = Vector3.SmoothDamp(transform.position, spawnPoint[0], ref velo, 0.5f);
             
         }
 
@@ -69,7 +71,7 @@ public class MovementPjw : MoveMent
                 Debug.Log("세이브포인트 02 : " + spawnPoint[1]);
             }
 
-            transform.position = Vector3.MoveTowards(transform.position, spawnPoint[1], speed);
+            transform.position = Vector3.SmoothDamp(transform.position, spawnPoint[1],ref velo, 0.5f);
             
 
         }
@@ -82,7 +84,7 @@ public class MovementPjw : MoveMent
                 spawnPoint[2] = this.transform.position;
                 Debug.Log("세이브포인트 03 : " + spawnPoint[2]);
             }
-            transform.position = Vector3.MoveTowards(transform.position, spawnPoint[2], speed);
+            transform.position = Vector3.SmoothDamp(transform.position, spawnPoint[2], ref velo,0.5f);
             
         }
 
@@ -95,7 +97,7 @@ public class MovementPjw : MoveMent
                 Debug.Log("세이브포인트 04 : " + spawnPoint[3]);
             }
 
-            transform.position = Vector3.MoveTowards(transform.position, spawnPoint[3], speed);
+            transform.position = Vector3.SmoothDamp(transform.position, spawnPoint[3],ref velo, 0.5f);
             
         }
 
