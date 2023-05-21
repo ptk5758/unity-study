b29d7ef8a1582f6bb5116b82a9455542e8cdd899
asy <asy951013@naver.com>
2023-05-21 19:57:08 +0900

    asy

--- Assets/Script/movement/Movementasy.cs
+++ Assets/Script/movement/Movementasy.cs
@@ -56,9 +56,19 @@ public class Movementasy : MoveMent
             {
                 spawnPoint[0] = this.transform.position;
             }
-            this.transform.position = Vector3.MoveTowards(this.transform.position, spawnPoint[0], speed);
-            positionX = spawnPoint[0].x;
-            positionZ = spawnPoint[0].z;
+            Vector3 temp = Vector3.MoveTowards(this.transform.position, spawnPoint[0], speed);
+            //temp라는 새로운 변수를 만들어서 거기다가 spawnPoint[0]으로 가는 도중의 위치를 저장해둠(spawnPoint[0]에 완전히 도착하면 그곳의 위치를 중간에 가다가 멈추면
+            //멈춘 위치의 좌표를 저장해둬서 중간에 가다가 멈춰서 WASD를 눌러도 spawnPoint[0]에 저장해둔 위치로 가지 않고 멈춘 위치에서 움직여짐
+            positionX = temp.x; //temp.x의 값을 positionX에 저장
+            positionZ = temp.z; ////temp.Z의 값을 positionZ에 저장
+            this.transform.position = new Vector3(positionX, this.transform.position.y, positionZ);
+            //현재 위치를 this.transform.position에다가 저장
+            //this.transform.position = Vector3.MoveTowards(this.transform.position, spawnPoint[0], speed);
+            //positionX = spawnPoint[0].x;
+            //positionZ = spawnPoint[0].z;
+            //이렇게 코드를 작성하면 spawnPoint[0]으로 이동하는 중에 중간에 멈춰서 WASD를 누르면 그 자리에서 움직이는 게 아니라 spawnPoint[0]까지 옮겨가서
+            //움직여지는 오류가 발생, positionX=spawnPoint[0].x로 코드를 작성하면 중간에 가다가 멈춘 자리의 X 좌표가 아니라 
+            //spawnPoint[0]에 저장되있는 X 좌표가 저장되기 때문에 spawnPoint[0]의 X 좌표로 옮겨가버림
         }
 
         if (Input.GetKey(KeyCode.Alpha2))
@@ -67,9 +77,10 @@ public class Movementasy : MoveMent
             {
                 spawnPoint[1] = this.transform.position;
             }
-            this.transform.position = Vector3.MoveTowards(this.transform.position, spawnPoint[1], speed);
-            positionX = spawnPoint[1].x;
-            positionZ = spawnPoint[1].z;
+            Vector3 temp = Vector3.MoveTowards(this.transform.position, spawnPoint[1], speed);
+            positionX = temp.x;
+            positionZ = temp.z;
+            this.transform.position = new Vector3(positionX, this.transform.position.y, positionZ);
         }
 
         if (Input.GetKey(KeyCode.Alpha3))
@@ -78,9 +89,10 @@ public class Movementasy : MoveMent
             {
                 spawnPoint[2] = this.transform.position;
             }
-            this.transform.position = Vector3.MoveTowards(this.transform.position, spawnPoint[2], speed);
-            positionX = spawnPoint[2].x;
-            positionZ = spawnPoint[2].z;
+            Vector3 temp = Vector3.MoveTowards(this.transform.position, spawnPoint[2], speed);
+            positionX = temp.x;
+            positionZ = temp.z;
+            this.transform.position = new Vector3(positionX, this.transform.position.y, positionZ);
         }
 
 
@@ -90,9 +102,10 @@ public class Movementasy : MoveMent
             {
                 spawnPoint[3] = this.transform.position;
             }
-            this.transform.position = Vector3.MoveTowards(this.transform.position, spawnPoint[3], speed);
-            positionX = spawnPoint[3].x;
-            positionZ = spawnPoint[3].z;
+            Vector3 temp = Vector3.MoveTowards(this.transform.position, spawnPoint[3], speed);
+            positionX = temp.x;
+            positionZ = temp.z;
+            this.transform.position = new Vector3(positionX, this.transform.position.y, positionZ);
         }
 
 
